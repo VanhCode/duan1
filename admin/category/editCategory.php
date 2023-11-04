e46e605e4430bdd1f0b67b8a773a5564d9a7ee51
vanh <tranvanh2k4@gmail.com>
2023-11-04 13:19:39 +0700

    fl

--- admin/category/editCategory.php
+++ admin/category/editCategory.php
@@ -57,12 +57,12 @@
                     <i class="bx bx-filter"></i>
                 </div>
 
-                <form action="index.php?action=updateCategory" method="post">
+                <form action="index.php?action=editCategory&category_id=<?= $category_id ?>" method="post">
                     <div class="form__cateadd">
                         <input type="hidden" name="id" value="<?= $category_id ?>">
                         <div class="form-group">
                             <label class="form-label">Tên danh mục</label>
-                            <input type="text" placeholder="Nhập tên danh mục" value="<?= $category_name ?>" name="danhmuc" class="form-control">
+                            <input type="text" placeholder="Nhập tên danh mục" value="<?= !empty($category_name) ? $category_name : "" ?>" name="danhmuc" class="form-control">
                         </div>
                         <div class="error error_category">
                             <?= isset($errCategory) ? $errCategory : "" ?>
