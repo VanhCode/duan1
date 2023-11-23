876a030e5ad63747d54524d6f1bd81d7bbcb2ade
vanh <tranvanh2k4@gmail.com>
2023-11-23 21:49:32 +0700

    push

--- admin/order/listOrder.php
+++ admin/order/listOrder.php
@@ -22,7 +22,9 @@
     </nav>
     <!-- NAVBAR -->
     <style>
-        th, td, p {
+        th,
+        td,
+        p {
             text-align: left !important;
         }
     </style>
@@ -57,77 +59,87 @@
                 </div>
                 <table>
                     <thead>
-                    <tr>
-                        <th style="padding-right: 20px">STT</th>
-                        <th>Người đặt hàng</th>
-                        <th>Người nhận</th>
-                        <th>Tổng cộng</th>
-                        <th>Ngày đặt hàng</th>
-                        <th>Trạng thái</th>
-                        <th>Chi tiết</th>
-                    </tr>
+                        <tr>
+                            <th style="padding-right: 20px">STT</th>
+                            <th>Người đặt hàng</th>
+                            <th>Người nhận</th>
+                            <th>Tổng cộng</th>
+                            <th>Ngày đặt hàng</th>
+                            <th>Trạng thái</th>
+                            <th>Thanh toán</th>
+                            <th>Chi tiết</th>
+                        </tr>
                     </thead>
                     <tbody>
-                    <?php foreach ($listOrder as $key => $value): ?>
-                        <tr>
-                            <td style="margin-top: 45px"><?= $key + 1 ?></td>
-                            <td>
-                                <img src="../public/upload/image/user/<?= $value['user_image'] ?>" alt="">
-                                <span><?= $value['fullName'] ?></span>
-                            </td>
-                            <td>
-                                <p><i><?= $value['receiver_name'] ?></i></p>
-                                <p style="width: 150px;"><b>ĐC: </b><?= $value['receiver_address'] ?></p>
-                                <p><b>SĐT: </b><?= $value['receiver_phone'] ?></p>
-                            </td>
-                            <td style="color: #ff7d7d;font-weight: 500;"><?= number_format($value['total'], 0, ',', '.') ?></td>
-                            <td><?= $value['create_at'] ?></td>
+                        <?php foreach ($listOrder as $key => $value) : ?>
+                            <tr>
+                                <td style="margin-top: 45px"><?= $key + 1 ?></td>
+                                <td>
+                                    <img src="../public/upload/image/user/<?= $value['user_image'] ?>" alt="">
+                                    <span><?= $value['fullName'] ?></span>
+                                </td>
+                                <td>
+                                    <p><i><?= $value['receiver_name'] ?></i></p>
+                                    <p style="width: 150px;"><b>ĐC: </b><?= $value['receiver_address'] ?></p>
+                                    <p><b>SĐT: </b><?= $value['receiver_phone'] ?></p>
+                                </td>
+                                <td style="color: #ff7d7d;font-weight: 500;"><?= number_format($value['total'], 0, ',', '.') ?></td>
+                                <td><?= $value['create_at'] ?></td>
 
-                            <td>
-                                <select onchange="changeStatus(this,<?=$value['order_id']?>)"
-                                        class="form-select-sm selected_status" name="status"
-                                        <?=$value['status']=='completed'?'disabled':''?>
-                                >
-                                    <?php
-                                        $status = 
-                                                [
-                                                    'pending' => 'Chờ xác nhận',
-                                                    'confirmed' => 'Đã xác nhận', 
-                                                    'shipping' => 'Đang vận chuyển', 
-                                                    'completed' => 'Hoàn thành'
-                                                ];
-                                        foreach ($status as $key => $value):?>
-                                            <option <?= $key == $order['status'] ? 'selected' : '' ?>
-                                                    style="font-size: 14px; padding: 5px" class="status"
-                                                    value="<?= $key ?>"><?= $value ?>    
+                                <td>
+                                    <select onchange="changeStatus(this,<?= $value['order_id'] ?>)" class="form-select-sm selected_status" name="status" <?= $value['status'] == 'completed' ? 'disabled' : '' ?>>
+                                        <?php
+                                        $status =
+                                            [
+                                                'pending' => 'Chờ xác nhận',
+                                                'confirmed' => 'Đã xác nhận',
+                                                'shipping' => 'Đang vận chuyển',
+                                                'completed' => 'Hoàn thành'
+                                            ];
+                                        foreach ($status as $key => $order) : ?>
+                                            <option <?= $key == $value['status'] ? 'selected' : '' ?> style="font-size: 14px; padding: 5px" value="<?= $key ?>"><?= $order ?>
                                             </option>
                                         <?php endforeach; ?>
-                                </select>
-                            </td>
+                                    </select>
+                                </td>
+                                <td>
+                                    <select onchange="changeStatus(this,<?= $value['order_id'] ?>,'payment_status')" class="form-select-sm selected_status" name="status" <?= $value['payment_status'] == 'completed' ? 'disabled' : '' ?>>
+                                        <?php
+                                        $status =
+                                            [
+                                                'unpaid' => 'Chưa thanh toán',
+                                                'paid' => 'Đã thanh toán',
+                                                'repaid' => 'Yêu cầu hoàn tiền'
+                                            ];
+                                        foreach ($status as $key => $order) : ?>
+                                            <option <?= $key == $value['payment_status'] ? 'selected' : '' ?> style="font-size: 14px; padding: 5px" value="<?= $key ?>"><?= $order ?>
+                                            </option>
+                                        <?php endforeach; ?>
+                                    </select>
+                                </td>
 
-                            <td>
-                                <a class="btn btn-success btn-sm"
-                                   href="index.php?action=listOrder_detail&order_id=<?= $value['order_id'] ?>">Chi
-                                    tiết</a>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
+                                <td>
+                                    <a class="btn btn-success btn-sm" href="index.php?action=listOrder_detail&order_id=<?= $value['order_id'] ?>">Chi
+                                        tiết</a>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
                     </tbody>
                 </table>
                 <script>
-                    function changeStatus(select,order_id) {
+                    function changeStatus(select, order_id, action = 'status') {
                         console.log("Sending request with status:", select.value, "and order_id:", order_id);
-                        if(select.disabled===false){
+                        if (select.disabled === false) {
                             let xmlHttp = new XMLHttpRequest();
-                            xmlHttp.onreadystatechange = function () {
+                            xmlHttp.onreadystatechange = function() {
                                 if (this.readyState === 4 && this.status === 200) {
                                     select.value = this.responseText
-                                    if(this.responseText==='completed'){
-                                        select.disabled=true;
+                                    if (this.responseText === 'completed') {
+                                        select.disabled = true;
                                     }
                                 }
                             }
-                            xmlHttp.open('GET', `./xmlHttpRequest/statusOrder.php?status=${select.value}&order_id=${order_id}`, true);
+                            xmlHttp.open('GET', `./xmlHttpRequest/statusOrder.php?${action}=${select.value}&order_id=${order_id}`, true);
                             xmlHttp.send();
                         }
                     }
--- admin/xmlHttpRequest/statusOrder.php
+++ admin/xmlHttpRequest/statusOrder.php
@@ -1,9 +1,19 @@
 <?php
 include '../../models/PDO_admin.php';
-$status=$_GET['status']??'pending';
 $order_id=$_GET['order_id']??0;
-updateData('orders',[
-    'status'=>$status
-],'order_id='.$order_id);
-echo $status;
+if(isset($_GET['status'])){
+    $status=$_GET['status']??'pending';
+    updateData('orders',[
+        'status'=>$status
+    ],'order_id='.$order_id);
+    echo $status;
+}
+
+if(isset($_GET['payment_status'])){
+    $status=$_GET['payment_status']??'pending';
+    updateData('orders',[
+        'payment_status'=>$status
+    ],'order_id='.$order_id);
+    echo $status;
+}
 ?>
