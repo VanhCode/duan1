86d71500d6fc46d852a4d443ec633714cfc8b39f
duynndu <duynnz2812@gmail.com>
2023-11-24 16:01:20 +0700

    xoá rác

--- duan1-master/giaoDien/php/flashSale.php
+++
@@ -1,8 +0,0 @@
-<?php
-    require "./connect/connect.php";
-
-    $queryFlash = "SELECT * FROM sanphambanchay LIMIT 16";
-    $stateFlash = $db->prepare($queryFlash);
-    $stateFlash->execute();
-    $resultFlash = $stateFlash->fetchAll(PDO::FETCH_ASSOC);
-?>
