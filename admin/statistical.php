ba54f9799ccc68027c3332e3d2b91167e0126c40
duynndu <duynnz2812@gmail.com>
2023-11-30 02:01:17 +0700

    statistical.php

--- admin/statistical.php
+++ admin/statistical.php
@@ -1,3 +1,17 @@
+<style>
+    .form-group {
+        width: 40%;
+    }
+
+    .form-group label {
+        width: 70px;
+        padding-right: 10px;
+    }
+
+    .form-group input {
+        margin-right: 10px;
+    }
+</style>
 <section id="content">
     <!-- NAVBAR -->
     <nav>
@@ -46,7 +60,6 @@
         </div>
 
 
-
         <div class="table-data">
             <div class="order">
                 <div class="head">
@@ -54,6 +67,27 @@
                     <i class='bx bx-search'></i>
                     <i class='bx bx-filter'></i>
                 </div>
+                <form class="d-flex align-items-center form-statistical">
+                    <div class="form-group d-flex justify-content-center align-items-center">
+                        <label for="">Từ</label>
+                        <input class="form-control from" type="datetime-local">
+                        <label for="">Đến</label>
+                        <input class="form-control to" type="datetime-local">
+                    </div>
+                    <div class="form-group d-flex align-items-center">
+                        <select class="form-select w-50 type mx-2">
+                            <option hidden="hidden" value="DATE">Thống kê theo</option>
+                            <option value="DATE">Ngày</option>
+                            <option value="WEEK">Tuần</option>
+                            <option value="MONTH">Tháng</option>
+                            <option value="YEAR">Năm</option>
+                        </select>
+                    </div>
+                    <div class="form-group" style="text-align: right">
+                        <button class="btn btn-success send-data" type="button">Lọc</button>
+                    </div>
+                </form>
+
                 <canvas id="myChart"></canvas>
             </div>
         </div>
@@ -62,46 +96,117 @@
 </section>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script>
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var chart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-            datasets: [
-            {
-                label: 'Số đơn hàng',
-                data: [12, 19, 3, 5, 2, 3, 7],
-                backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                borderColor: 'rgba(255, 99, 132, 1)',
-                borderWidth: 1
-            },
-            {
-                label: 'Doanh thu',
-                data: [1000, 2000, 3000, 4000, 5000, 6000, 7000],
-                backgroundColor: 'rgba(255, 206, 86, 0.2)',
-                borderColor: 'rgba(255, 206, 86, 1)',
-                borderWidth: 1,
-                yAxisID: 'y-axis-1'
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    },
-                    id: 'y-axis-0',
-                    position: 'left'
-                }, {
-                    ticks: {
-                        beginAtZero: true
-                    },
-                    id: 'y-axis-1',
-                    position: 'right'
-                }]
+    let form_statistical = document.querySelector('.form-statistical');
+    let btn_send_data = document.querySelector('.send-data');
+    let xmlHttp = new XMLHttpRequest();
+    let labels=[];
+    let numOrder=[];
+    let revenue=[];
+    btn_send_data.addEventListener('click', function () {
+        let from = form_statistical.querySelector('.from');
+        let to = form_statistical.querySelector('.to');
+        let type = form_statistical.querySelector('.type');
+        let data = `from=${from.value}&to=${to.value}&type=${type.value}`;
+        xmlHttp.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                dataReturn=JSON.parse(this.responseText);
+                dataReturn.revenue=dataReturn.revenue?.map((value)=>(parseInt(value)));
+                dataReturn.numOrder=dataReturn.numOrder?.map((value)=>(parseInt(value)));
+                updateChart(chart, dataReturn.date, dataReturn.numOrder, dataReturn.revenue);
             }
         }
+        xmlHttp.open('POST','xmlHttpRequest/chart.php');
+        xmlHttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xmlHttp.send(data);
     });
+    getDataChart()
+        .then((dt)=>{
+            dt.revenue=dt.revenue?.map((value)=>(parseInt(value)));
+            dt.numOrder=dt.numOrder?.map((value)=>(parseInt(value)));
+            return dt;
+        })
+        .then((res)=>{
+            updateChart(chart,res.date,res.numOrder,res.revenue);
+        })
+        .catch((err)=>{
+            console.error('có lỗi '+err);
+        });
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var chart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: [],
+                datasets: [
+                    {
+                        label: 'Số đơn hàng',
+                        data: [],
+                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                        borderColor: 'rgba(255, 99, 132, 1)',
+                        borderWidth: 1
+                    },
+                    {
+                        label: 'Doanh thu',
+                        data: [],
+                        backgroundColor: 'rgba(255, 206, 86, 0.2)',
+                        borderColor: 'rgba(255, 206, 86, 1)',
+                        borderWidth: 1,
+                        yAxisID: 'y-axis-1'
+                    }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        },
+                        id: 'y-axis-0',
+                        position: 'left'
+                    }, {
+                        ticks: {
+                            beginAtZero: true
+                        },
+                        id: 'y-axis-1',
+                        position: 'right'
+                    }]
+                }
+            }
+        });
+    function updateChart(chart, date, numOrder, revenue) {
+        chart.data.datasets[0].data=numOrder;
+        chart.data.datasets[1].data=revenue;
+        chart.data.labels=date
+        chart.update();
+    }
+    function getDataChart() {
+        return new Promise((resolve, reject) => {
+            let dataR={};
+            let fromDate=new Date();
+            fromDate = fromDate.getFullYear() + '-' + (fromDate.getMonth() ) + '-' +
+                fromDate.getDate() + 'T' +
+                fromDate.getHours() + ':' +
+                fromDate.getMinutes()
+
+            let toDate = new Date();
+            toDate=toDate.getFullYear() + '-' + (toDate.getMonth() + 1) + '-' +
+                toDate.getDate() + 'T' +
+                toDate.getHours() + ':' +
+                toDate.getMinutes()
+            let data = `from=${fromDate}&to=${toDate}&type=DATE`;
+
+            xmlHttp.onreadystatechange = function () {
+                if (this.readyState === 4 && this.status === 200) {
+                    dataR = JSON.parse(this.responseText);
+                    resolve(dataR); // Resolve the promise with the data
+                } else if (this.readyState === 4) {
+                    reject(this.status); // Reject the promise if there's an error
+                }
+            }
+
+            xmlHttp.open('POST','xmlHttpRequest/chart.php');
+            xmlHttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+            xmlHttp.send(data);
+        });
+    }
 </script>
 
 
--- admin/xmlHttpRequest/chart.php
+++ admin/xmlHttpRequest/chart.php
@@ -1,8 +1,6 @@
 <?php
 include "../../models/PDO_admin.php";
-
 function statistical($from,$to,$type='DATE'){
-
     $sql="WITH RECURSIVE dates AS (
       SELECT DATE('$from') AS date
       UNION ALL
@@ -11,14 +9,17 @@ function statistical($from,$to,$type='DATE'){
       WHERE DATE_ADD(date, INTERVAL 1 DAY) <= '$to'
     )
     ";
-    $sql.="SELECT dates.date, COUNT(orders.order_id) AS num_orders, SUM(order_details.amount * order_details.price) AS revenue
+    $sql.="SELECT ".($type == 'MONTH' ? "DATE_FORMAT(dates.date, '%Y-%m')" : "$type(dates.date)")." AS date, COUNT(orders.order_id) AS num_orders, SUM(order_details.amount * order_details.price) AS revenue
     FROM dates
-    LEFT JOIN orders ON $type(orders.create_at) = $type(dates.date)
+    LEFT JOIN orders ON DATE(orders.create_at) = DATE(dates.date)
     LEFT JOIN order_details ON order_details.order_id=orders.order_id
     GROUP BY $type(dates.date)";
     return query($sql)->fetchAll();
 }
-$data=statistical('2023-11-01','2023-12-31','MONTH');
+$data=statistical($_POST['from']??'2023-10-11',$_POST['to']??'2023-11-30',$_POST['type']??'DATE');
+$date=[];
+$numOrder=[];
+$revenue=[];
 foreach ($data as $key=>$value){
     if($value['num_orders']==null){
         $data[$key]['num_orders']=0;
@@ -26,7 +27,12 @@ foreach ($data as $key=>$value){
     if($value['revenue']==null){
         $data[$key]['revenue']=0;
     }
+    $date[]=$value['date']??0;
+    $numOrder[]=$value['num_orders']??0;
+    $revenue[]=$value['revenue']??0;
 }
-echo "<pre>";
-print_r($data);
-//echo json_encode($data);
+$data=[];
+$data['date']=$date;
+$data['numOrder']=$numOrder;
+$data['revenue']=$revenue;
+echo json_encode($data);
