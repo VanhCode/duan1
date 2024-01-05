ea15cda2986a2858ebe61ba6a514bef2d3698929
duynndu <duynnz2812@gmail.com>
2024-01-06 00:20:29 +0700

    push lần cuối

--- models/userModel/productModel.php
+++ models/userModel/productModel.php
@@ -158,7 +158,7 @@
     // Group lấy màu sắc 
     function listVariationColor($id_product) {
         $sql = "SELECT 
-                    variant_id,
+                    MIN(variant_id) AS variant_id, 
                     product_id,
                     color,
                     GROUP_CONCAT(size) AS size,
@@ -166,7 +166,7 @@
                 FROM 
                     variants
                 WHERE 
-                    product_id = '$id_product' GROUP BY color,variant_id";
+                    product_id = '$id_product' GROUP BY color";
         return pdo_query($sql);
     }    
 
@@ -174,14 +174,14 @@
     // Group lấy size 
     function listVariationSize($id_product) {
         $sql = "SELECT 
-                    variant_id,
+                    MIN(variant_id) AS variant_id, 
                     product_id,
                     GROUP_CONCAT(size) AS size,
                     GROUP_CONCAT(amount) AS amount
                 FROM 
                     variants
                 WHERE 
-                    product_id = '$id_product' GROUP BY product_id,variant_id";
+                    product_id = '$id_product' GROUP BY product_id";
         return pdo_query_one($sql);
     }
 
